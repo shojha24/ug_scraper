dcb95da700d5dc43050446a268d7e595202990fa
Sharabh Ojha <79208016+shojha24@users.noreply.github.com>
2025-07-25 19:15:40 -0400

    condensed all scraped chords to one json

--- helper.py
+++ helper.py
@@ -16,4 +16,31 @@ def create_set_from_all_json_files():
     with open('scraped_data/unique_links.json', 'w') as outfile:
         json.dump(list(unique_items), outfile, indent=2)
 
-create_set_from_all_json_files()
+def combine_all_chords():
+    """
+    Combine all unique chords from the JSON files in the 'all_chords' directory into a single set.
+    """
+    all_chords = {}
+
+    for filename in os.listdir('all_chords'):
+        if filename.endswith('.json'):
+            with open(f'all_chords/{filename}', 'r') as file:
+                data = json.load(file)
+                for chord, details in data.items():
+                    if chord not in all_chords:
+                        all_chords[chord] = details
+                    else:
+                        # If the chord already exists, merge the details
+                        all_chords[chord].update(details)
+
+    with open('all_chords/all_chords.json', 'w') as outfile:
+        json.dump(all_chords, outfile, indent=2)
+
+
+
+if __name__ == "__main__":
+    # create_set_from_all_json_files()
+    combine_all_chords()
+
+
+
